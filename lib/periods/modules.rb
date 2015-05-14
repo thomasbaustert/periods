e4acaab053a8975e29995c86cdb7379fd43ce9d2
Thomas Baustert <business@thomasbaustert.de>
2015-05-14 20:35:57 +0200

    provide file to load all modules

---
+++ lib/periods/modules.rb
@@ -0,0 +1,12 @@
+require 'periods/modules/period'
+require 'periods/modules/month'
+require 'periods/modules/monthly_period'
+require 'periods/modules/quarter'
+require 'periods/modules/quarterly_period'
+require 'periods/modules/halfyear'
+require 'periods/modules/halfyearly_period'
+require 'periods/modules/year'
+require 'periods/modules/yearly_period'
+
+
+
