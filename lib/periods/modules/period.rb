6df7a3a1517de448a89802ef8a5fa05afe7371bb
Thomas Baustert <business@thomasbaustert.de>
2015-05-17 18:12:06 +0200

    add yardoc for Period

--- lib/periods/modules/period.rb
+++ lib/periods/modules/period.rb
@@ -10,17 +10,54 @@ module Periods
       end
 
       module InstanceMethods
-        attr_reader :start_date, :end_date
 
+        # Returns the start date of this period.
+        # @return [Date] the start date of this period.
+        attr_reader :start_date
+
+        # Returns the end date of this period.
+        # @return [Date] the end date of this period.
+        attr_reader :end_date
+
+        ##
+        # Initialize a new period with its start and end date.
+        #
+        # @example
+        #
+        #    period = Period.new('10.04.2015', '20.05.2015')
+        #
         def initialize(start_date, end_date)
           @start_date = Date.parse(start_date.to_s)
           @end_date = Date.parse(end_date.to_s)
         end
 
+        ##
+        # Test whether the given period is equal to this one.
+        # A period is considered equal if it has the same start and end date.
+        #
+        # @return [Boolean] true if period is equal, false otherwise.
+        #
+        # @since 0.0.3
+        #
+        # @example
+        #
+        #    period1 = Period.new('10.04.2015', '20.05.2015')
+        #    period2 = Period.new('10.04.2015', '20.05.2015')
+        #    period3 = Period.new('09.04.2015', '20.05.2015')
+        #    period1 == period2 # => true
+        #    period1 == period3 # => false
+        #
         def ==(period)
           start_date == period.start_date && end_date == period.end_date
         end
 
+        ##
+        # Test whether the given period is older, newer or equal to this one.
+        #
+        # @return [Fixnum] 1 if period is older, -1 if newer, 0 if equal.
+        #
+        # @since 0.0.3
+        #
         def <=>(period)
           if start_date > period.start_date
             1
@@ -31,18 +68,76 @@ module Periods
           end
         end
 
+        ##
+        # Returns next period.
+        #
+        # @return [Period] the next period.
+        #
+        # @since 0.0.3
+        #
+        # @see Periods::Modules::Period#previous
+        #
+        # @example
+        #
+        #    period = Period.new('10.04.2015', '20.05.2015')
+        #    period.next # => 21.05.2015 - 30.06.2015
+        #
         def next
           self.class.new(start_date + days, end_date + days)
         end
 
+        ##
+        # Returns previous period.
+        #
+        # @return [Period] the previous period.
+        #
+        # @since 0.0.3
+        #
+        # @see Periods::Modules::Period#next
+        #
+        # @example
+        #
+        #    period = Period.new('10.04.2015', '20.05.2015')
+        #    period.previous # => 28.02.2015 - 09.04.2015
+        #
         def previous
           self.class.new(start_date - days, end_date - days)
         end
 
+        ##
+        # Returns number of days in period.
+        #
+        # @return [Fixnum] the number of days in period.
+        #
+        # @since 0.0.3
+        #
+        # @example
+        #
+        #    Period.new('10.04.2015', '20.05.2015').days # => 41
+        #
         def days
           end_date.yday - start_date.yday + 1
         end
 
+        ##
+        # Test whether the given date or period is included in this one.
+        #
+        # @return [Boolean] true if date or period is included, false otherwise.
+        #
+        # @since 0.0.3
+        #
+        # @example
+        #
+        #    Period.new('10.04.2015', '20.05.2015').include?('10.04.2015') # => true
+        #    Period.new('10.04.2015', '20.05.2015').include?('20.05.2015') # => true
+        #    Period.new('10.04.2015', '20.05.2015').include?('09.04.2015') # => false
+        #    Period.new('10.04.2015', '20.05.2015').include?('21.05.2015') # => false
+        #
+        #    Period.new('10.04.2015', '20.05.2015').include?(Period.new('10.04.2015', '20.05.2015')) # => true
+        #    Period.new('10.04.2015', '20.05.2015').include?(Period.new('15.04.2015', '15.05.2015')) # => true
+        #    Period.new('10.04.2015', '20.05.2015').include?(Period.new('09.04.2015', '20.05.2015')) # => false
+        #    Period.new('10.04.2015', '20.05.2015').include?(Period.new('10.04.2015', '21.05.2015')) # => false
+        #
         def include?(period)
           if period.is_a?(Date)
             start_date <= period && period <= end_date
@@ -56,6 +151,17 @@ module Periods
           end
         end
 
+        ##
+        # Return string representation of period.
+        #
+        # @return [String] the string representation of period.
+        #
+        # @since 0.0.3
+        #
+        # @example
+        #
+        #    Period.new('10.04.2015', '20.05.2015').to_s # => '10.04.2015 -  20.05.2015'
+        #
         def to_s
           "#{start_date.strftime("%d.%m.%Y")} - #{end_date.strftime("%d.%m.%Y")}"
         end
